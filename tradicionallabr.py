633dd208d6f6c673b87ae65227d3ab7cdd97f022
jbalvin03 <mpyf83702@gmail.com>
2024-04-09 19:38:42 -0500

    nt8 feat se crea ejemplo de funcion tradional

---
+++ tradicionallabr.py
@@ -0,0 +1,21 @@
+'''numeroUno=5
+numeroDos=10
+sumatoria=numeroUno+numeroDos
+print(f"La suma es {sumatoria}")
+
+numeroTres=2
+numeroCuatro=10
+sumatoriaDos=numeroTres+numeroCuatro
+print(f"La suma es {sumatoriaDos}")'''
+
+#Declarando una funcion tradicional 
+def sumarNumeros(numeroUno,numeroDos):
+    sumatoria=numeroUno+numeroDos
+    return sumatoria
+
+#Llamando una funcion tradicional 
+numeroUno=5
+numeroDos=20
+numeroTres=50
+resultado=sumarNumeros(15,2000)
+print(f"La suma es: {resultado}")
